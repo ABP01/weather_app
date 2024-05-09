93165c24be9ed3dee33201a2556efdc1aa164faa
Armel Bogue <komlanbogue@gmail.com>
2024-05-09 14:34:51 +0000

    Request

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:geocoding/geocoding.dart';
-
+import 'package:geolocator/geolocator.dart';
 import '../models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -27,16 +27,15 @@ class WeatherService {
     //get permission from user
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
-      permission = await Geolocator.resquestPermission();
+      permission = await Geolocator.requestPermission();
     }
 
     // fetch the current location
     Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
-    )
+        desiredAccuracy: LocationAccuracy.high);
 
     //convert the location into a list of placemark objects
-    List<Placemark> placemarks = 
+    List<Placemark> placemarks =
         await placemarkFromCoordinates(position.latitude, position.longitude);
     //extract the city name from the first placemark
 
