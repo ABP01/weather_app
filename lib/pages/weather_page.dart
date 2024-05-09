8b50f7a50c658447aeeed40532f64e6819630d30
Armel Bogue <komlanbogue@gmail.com>
2024-05-09 15:54:22 +0000

    Api key not working

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +10,54 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  // api key
+  final _weatherService = WeatherService('2cbf46a7f2e1436b2813e995d8820716');
+  Weather? _weather;
+  //fetch weather
+  _fetchWeather() async {
+    //get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+
+    //any errors
+    catch (e) {
+      print(e);
+    }
+  }
+
+  // weather animations
+
+  // init state
+  @override
+  void initState() {
+    super.initState();
+
+    //fetch weather on startup
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //city name
+            Text(_weather?.cityName ?? "Loading city.."),
+
+            //temperature
+            Text('${_weather?.temperature.round()}°C)'),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,7 +6,7 @@ import '../models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
   WeatherService(this.apiKey);
